510ba5aaeb76f63bb784dc1f48b3fa0eacc9ece8
pratitb <pratit.bangdiwala7@gmail.com>
2026-05-11 19:07:33 +0530

    fix: card width

--- src/components/categoryCard.tsx
+++ src/components/categoryCard.tsx
@@ -3,7 +3,7 @@ import type { CategoryProps } from '../utils/types'
 
 const CategoryCard = ({ title, includes }: CategoryProps) => {
     return (
-        <div className='min-w-[170px] flex flex-col gap-2 border border-black p-4 rounded-lg bg-white shadow-xl'>
+        <div className='min-w-[180px] flex flex-col gap-2 grow border border-black p-4 rounded-lg bg-white shadow-xl'>
             <p className='font-semibold text-3xl capitalize'>{title}</p>
             <p className='grow'>{includes}</p>
             <div className='bg-black w-fit p-3 rounded-full self-end'><FaLocationArrow color='white' size={20} /></div>
