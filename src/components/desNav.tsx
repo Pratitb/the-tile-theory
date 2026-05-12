5d93f86b2b8e81b8718dc569aa8651640a40b657
pratitb <pratit.bangdiwala7@gmail.com>
2026-05-12 20:03:33 +0530

    fix: keys to mapped data

--- src/components/desNav.tsx
+++ src/components/desNav.tsx
@@ -5,7 +5,7 @@ const DesNav = ({ getActiveFn, activeSt }: NavProps) => {
     return (
         <div className='flex gap-2 py-2 px-2 bg-lightBg rounded-full'>
             {navLinks?.map((link: NavLinkKeys) =>
-                <div className='flex items-center gap-1'>
+                <div key={link?.name} className='flex items-center gap-1'>
                     <p className={`capitalize text-sm rounded-full px-3 py-2 cursor-pointer ${link?.name === activeSt ? 'bg-black text-white font-bold' : ''}`} onClick={() => getActiveFn(link?.name ?? 'home')}>{link?.name}</p>
                 </div>
             )}
--- src/components/mobNav.tsx
+++ src/components/mobNav.tsx
@@ -6,7 +6,7 @@ const MobNav = ({ getActiveFn, activeSt }: NavProps) => {
     return (
         <div className='flex justify-between bg-lightBg border-2 rounded-full px-2 py-2 fixed bottom-2 left-[50%] translate-x-[-50%]'>
             {navLinks?.map((link: NavLinkKeys) =>
-                <p className={`capitalize text-base rounded-full px-3 py-2 cursor-pointer ${link?.name === activeSt ? 'bg-black text-white font-bold' : ''}`} onClick={() => getActiveFn(link?.name ?? 'home')}>{link?.name}</p>
+                <p key={link?.name} className={`capitalize text-base rounded-full px-3 py-2 cursor-pointer ${link?.name === activeSt ? 'bg-black text-white font-bold' : ''}`} onClick={() => getActiveFn(link?.name ?? 'home')}>{link?.name}</p>
                 // <div className='flex flex-col items-center'>
                 //     <span className='text-lg'>{link?.icon}</span>
                 // </div>
