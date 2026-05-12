c28de644e137e675cd94c64df738da81c0846505
pratitb <pratit.bangdiwala7@gmail.com>
2026-05-12 14:50:48 +0530

    fix: banner style

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
 import { useMediaQuery } from 'react-responsive'
 import './App.css'
 import MobNav from './components/mobNav'
-import MobHeader from './components/mobHeader'
 import Banner from './components/banner'
 import { categories } from './utils/data'
 import CategoryCard from './components/categoryCard'
 import type { CategoryProps } from './utils/types'
+import DesNav from './components/desNav'
+import Button from './components/button'
+import Brand from './components/brand'
+import AOS from 'aos';
+import { useEffect, useState } from "react";
+
 
 const App = () => {
 
   const isDesktop = useMediaQuery({ query: '(min-width: 767px)' })
 
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+  }, []);
+
+  const [activeBtn, setActiveBtn] = useState('home')
+  const handleActiveBtn = (btnName: string) => {
+    setActiveBtn(btnName)
+  }
+
   return (
     <div className='flex flex-col gap-4 container'>
-      {!isDesktop && <MobNav />}
-      <MobHeader />
+      {!isDesktop && <MobNav getActiveFn={handleActiveBtn} activeSt={activeBtn} />}
+
+      {/* brand, des nav, quote */}
+      <div className='flex justify-between items-center'>
+        <Brand />
+        {isDesktop && <DesNav getActiveFn={handleActiveBtn} activeSt={activeBtn} />}
+        <Button btnName="quote" />
+      </div>
+
+      {/* Banner */}
       <Banner />
+
+      {/* Categories */}
       <div className='mt-12 flex flex-col gap-6'>
         <p className='head'>categories</p>
         <div className='flex overflow-x-auto gap-6'>
           {categories?.map((item: CategoryProps) => <CategoryCard title={item?.title} includes={item?.includes} />)}
         </div>
+      </div>
+
+      {/* Applications */}
+      <div className='mt-12 flex flex-col gap-6'>
+        <p className='head'>applications</p>
+
       </div>
     </div>
   )
--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,10 +1,17 @@
 import Button from "./button"
+import 'aos/dist/aos.css'
 
 const Banner = () => {
+
+
+
     return (
-        <div className='w-full h-[400px] lg:h-[550px] flex flex-col gap-6 bg-banner bg-cover bg-no-repeat bg-bottom rounded-lg p-8'>
-            <p className="max-w-[500px] text-white font-bold text-4xl md:text-5xl md:leading-[55px]">If you're looking for the best tiles in Australia, you're at the right place.</p>
-            <Button btnName="explore categories" />
+        <div className='w-full min-h-[400px] lg:h-[450px] flex flex-col gap-6 bg-banner bg-cover bg-no-repeat bg-bottom rounded-lg p-8'>
+            <p className="max-w-[500px] text-white font-bold text-3xl md:text-5xl md:leading-[55px]" data-aos='fade-zoom-in'>If you're looking for the best tiles in Australia, you're at the right place.</p>
+            <div className="flex flex-col gap-2 md:flex-row">
+                <Button btnName="categories" />
+                <Button btnName="use cases" />
+            </div>
         </div>
     )
 }
