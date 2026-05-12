da138f36184e163b97e1b720fd3f3fc7d761dcdb
pratitb <pratit.bangdiwala7@gmail.com>
2026-05-12 16:21:57 +0530

    fix: category card, banner height

--- src/components/categoryCard.tsx
+++ src/components/categoryCard.tsx
@@ -3,10 +3,12 @@ import type { CategoryProps } from '../utils/types'
 
 const CategoryCard = ({ title, includes }: CategoryProps) => {
     return (
-        <div className='min-w-[180px] flex flex-col gap-2 grow border border-black p-4 rounded-lg bg-white shadow-xl'>
-            <p className='font-semibold text-3xl capitalize'>{title}</p>
-            <p className='grow max-w-36 lg:max-w-44'>{includes}</p>
-            <div className='bg-black w-fit p-3 rounded-full self-end'><FaLocationArrow color='white' size={20} /></div>
+        <div className='w-full flex items-center justify-between gap-6 grow border border-black px-4 py-4 rounded-lg bg-white shadow-xl'>
+            <div>
+                <p className='font-semibold text-3xl capitalize'>{title}</p>
+                <p className=''>{includes}</p>
+            </div>
+            <div className='bg-black w-fit p-3 rounded-full'><FaLocationArrow color='white' size={20} /></div>
         </div>
     )
 }
