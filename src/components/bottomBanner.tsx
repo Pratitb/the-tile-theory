ddbf80fb00b37e429ef6f5bbf9c109e5601c270d
pratitb <pratit.bangdiwala7@gmail.com>
2026-05-13 10:44:45 +0530

    fix: bottom banner data variable

--- src/components/bottomBanner.tsx
+++ src/components/bottomBanner.tsx
@@ -3,7 +3,8 @@ import type { BottomBannerProps } from "../utils/types"
 const BottomBanner = ({ quote }: BottomBannerProps) => {
     return (
 
-        <div className="flex-1 flex items-center bg-gradient-to-r from-[rgba(0,0,0,0.6)] to-[rgba(0,0,0,0.8)] text-white shadow-md border rounded-lg p-4">
+        <div className="flex-1 flex items-center text-center bg-gray-950 text-white shadow-md border rounded-lg p-4">
+            {/* gradient-to-r from-[rgba(0,0,0,0.6)] to-[rgba(0,0,0,0.8)] */}
             <p className="text-xl font-bold mb-3">{quote}</p>
         </div>
         // gradient-to-r from-[rgba(0,0,0,0.7)] to-[rgba(0,0,0,0.4)]
