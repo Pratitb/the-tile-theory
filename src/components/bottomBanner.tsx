82d11f0846b2c7301cc177c4eebf7df7c7a708b9
pratitb <pratit.bangdiwala7@gmail.com>
2026-05-30 12:28:12 +0530

    fix: content, responsive: product, category cards, bottom banner

--- src/components/bottomBanner.tsx
+++ src/components/bottomBanner.tsx
@@ -3,7 +3,7 @@ import type { BottomBannerProps } from "../utils/types"
 const BottomBanner = ({ quote }: BottomBannerProps) => {
     return (
 
-        <div className="flex-1 flex items-center text-center bg-gray-950 text-white shadow-md border rounded-lg p-4">
+        <div className="max-w-[500px] flex-1 flex items-center text-center bg-gray-950 text-white shadow-md border rounded-lg p-4">
             {/* gradient-to-r from-[rgba(0,0,0,0.6)] to-[rgba(0,0,0,0.8)] */}
             <p className="text-xl font-bold mb-3">{quote}</p>
         </div>
--- src/components/categoryCard.tsx
+++ src/components/categoryCard.tsx
@@ -3,12 +3,12 @@ import type { CategoryProps } from '../utils/types'
 
 const CategoryCard = ({ title, includes }: CategoryProps) => {
     return (
-        <div className='w-full flex items-center justify-between gap-6 grow border border-black px-3 py-3 rounded-lg bg-white shadow-xl'>
+        <div className='w-full flex items-start justify-between gap-3 grow border border-black p-4 rounded-lg bg-white shadow-xl'>
             <div>
                 <p className='font-semibold text-3xl capitalize'>{title}</p>
                 <p className=''>{includes}</p>
             </div>
-            <div className='bg-black w-fit p-3 rounded-full'><FaLocationArrow color='white' size={20} /></div>
+            <div className='bg-black w-fit p-3 self-center rounded-full cursor-pointer'><FaLocationArrow color='white' size={20} /></div>
         </div>
     )
 }
