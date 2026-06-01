bd6d8f087fa4e2c452eece0924f9210a994984db
pratitb <pratit.bangdiwala7@gmail.com>
2026-06-01 20:21:00 +0530

    feat: common classes

--- src/components/productCard.tsx
+++ src/components/productCard.tsx
@@ -2,16 +2,16 @@ import type { ProductCardProps } from '../utils/types'
 
 const ProductCard = ({ imageUrl, name, type, sizes, quality }: ProductCardProps) => {
     return (
-        <div className={`w-full min-w-[220px] max-w-[220px] md:min-w-[250px] lg:min-w-[280px] xl:min-w-[300px] rounded-lg border px-2 py-2 relative flex flex-col gap-2`}>
-            <img src={imageUrl} className='rounded-lg object-cover object-center' alt="" />
-            <div className='flex flex-col min-h-[150px] lg:min-h-fit bg-lightBg rounded-lg px-3 py-4 font-semibold capitalize'>
-                <p className='text-xl leading-6'>{name}</p>
-                <p className=' text-secondaryText grow mt-1'>{type}</p>
+        <div className='product_card'>
+            <img src={imageUrl} className='product_image' alt="" />
+            <div className='product_details'>
+                <p className='product_name'>{name}</p>
+                <p className=' product_type'>{type}</p>
                 <div className='flex gap-2 max-w-fit overflow-x-auto mt-3'>
                     {sizes?.map((size) => <p key={size} className='border rounded-full px-2 py-1 text-secondaryText text-sm'>{size}</p>)}
                 </div>
             </div>
-            <p className='absolute top-5 right-5 bg-black text-white text-sm capitalize md:text-sm xl:text-base rounded-lg px-2 py-1 md:px-3'>{quality}</p>
+            <p className='product_quality'>{quality}</p>
         </div>
     )
 }
