8822350306bd316328618713d2c4b404c681dcf2
pratitb <pratit.bangdiwala7@gmail.com>
2026-06-01 20:21:49 +0530

    fix: banner adjustments

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -5,7 +5,7 @@ import 'aos/dist/aos.css'
 const Banner = ({ getCategoriesScrollFn, getApplicationScrollFn }: BannerProps) => {
 
     return (
-        <div className='w-full min-h-[350px] lg:h-[450px] flex flex-col gap-6 bg-banner bg-cover bg-no-repeat bg-bottom rounded-lg p-8 xl:p-10 xl:py-14'>
+        <div className='w-full min-h-[350px] lg:h-[450px] flex flex-col gap-6 bg-banner bg-cover bg-no-repeat bg-bottom rounded-lg p-6 xl:p-10 xl:py-14'>
             <p className="max-w-[500px] text-white font-bold text-3xl md:text-5xl md:leading-[55px]" data-aos='fade-zoom-in'>If you're looking for the best tiles in Australia, you're at the right place.</p>
             <div className="flex flex-col gap-2 md:flex-row">
                 <Button btnName="categories" getActionFn={getCategoriesScrollFn} />
--- src/components/bottomBanner.tsx
+++ src/components/bottomBanner.tsx
@@ -3,9 +3,9 @@ import type { BottomBannerProps } from "../utils/types"
 const BottomBanner = ({ quote }: BottomBannerProps) => {
     return (
 
-        <div className="max-w-[500px] flex-1 flex items-center text-center bg-gray-950 text-white shadow-md border rounded-lg p-4">
+        <div className="flex flex-1 items-center text-center bg-gray-950 text-white shadow-md border rounded-lg px-[10px] py-4 lg:px-5">
             {/* gradient-to-r from-[rgba(0,0,0,0.6)] to-[rgba(0,0,0,0.8)] */}
-            <p className="text-xl font-bold mb-3">{quote}</p>
+            <p className=" font-bold mb-3">{quote}</p>
         </div>
         // gradient-to-r from-[rgba(0,0,0,0.7)] to-[rgba(0,0,0,0.4)]
     )
