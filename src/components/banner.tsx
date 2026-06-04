5d129e2722992b8f04eea49905f1304c012ed61d
pratitb <pratit.bangdiwala7@gmail.com>
2026-06-04 13:30:31 +0530

    feat: pills key, footer spacing, banner dynamic text

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -2,11 +2,11 @@ import type { BannerProps } from "../utils/types"
 import Button from "./button"
 import 'aos/dist/aos.css'
 
-const Banner = ({ getCategoriesScrollFn, getApplicationScrollFn }: BannerProps) => {
+const Banner = ({ bannerText, getCategoriesScrollFn, getApplicationScrollFn }: BannerProps) => {
 
     return (
         <div className='w-full min-h-[350px] lg:h-[450px] flex flex-col gap-6 bg-banner bg-cover bg-no-repeat bg-bottom rounded-lg p-6 xl:p-10 xl:py-14'>
-            <p className="max-w-[500px] text-white font-bold text-3xl md:text-5xl md:leading-[55px]" data-aos='fade-zoom-in'>If you're looking for the best tiles in Australia, you're at the right place.</p>
+            <p className="max-w-[500px] text-white font-bold text-3xl md:text-5xl md:leading-[55px]" data-aos='fade-zoom-in'>{bannerText}</p>
             <div className="flex flex-col gap-2 md:flex-row">
                 <Button btnName="categories" getActionFn={getCategoriesScrollFn} />
                 <Button btnName="applications" getActionFn={getApplicationScrollFn} />
--- src/components/pills.tsx
+++ src/components/pills.tsx
@@ -3,7 +3,7 @@ import type { PillsProps } from '../utils/types'
 const Pills = ({ getPills, getActivePill, activeSt }: PillsProps) => {
     return (
         <div className='flex gap-2 max-w-fit overflow-x-auto'>
-            {getPills?.map((pill: string) => <div className={`border border-black rounded-full capitalize whitespace-nowrap px-4 py-1 cursor-pointer ${activeSt === pill ? 'bg-black text-white font-bold' : ''}`} onClick={() => getActivePill?.(pill ?? '')}>{pill}</div>)}
+            {getPills?.map((pill: string) => <div key={pill} className={`border border-black rounded-full capitalize whitespace-nowrap px-4 py-1 cursor-pointer ${activeSt === pill ? 'bg-black text-white font-bold' : ''}`} onClick={() => getActivePill?.(pill ?? '')}>{pill}</div>)}
         </div>
     )
 }
