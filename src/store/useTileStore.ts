51e8b5ebac5fd3b76c831c5230dc098d60377ac6
pratitb <pratit.bangdiwala7@gmail.com>
2026-06-05 09:16:27 +0530

    feat: zustore active states

--- src/store/useTileStore.ts
+++ src/store/useTileStore.ts
@@ -6,11 +6,15 @@ interface TileStoreProps {
     updateActivePage: (page: string) => void
     updateTileMenuVisibility: () => void
     setTileMenuVisibility: (value: boolean) => void
+    activeTile: string
+    updateActiveTile: (tile: string) => void
 }
 export const useTileStore = create<TileStoreProps>((set) => ({
     activePage: 'home',
     toggleTileMenu: false,
+    activeTile: '',
     updateActivePage: (page) => set({ activePage: page }),
     updateTileMenuVisibility: () => set((state) => ({ toggleTileMenu: !state.toggleTileMenu })),
     setTileMenuVisibility: (value: boolean) => set({ toggleTileMenu: value }),
+    updateActiveTile: (tile: string) => set({ activeTile: tile }),
 }))
