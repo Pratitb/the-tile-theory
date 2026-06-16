7c644eb3c908fd3085c80fa0eaa4bb64f8eb9abd
pratitb <pratit.bangdiwala7@gmail.com>
2026-06-16 16:02:07 +0530

    fix: footer, banner spacing

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -14,7 +14,7 @@ interface BannerProps {
 const Banner = ({ text1, text2, text3, getCategoriesScrollFn, getApplicationScrollFn, categ, apps }: BannerProps) => {
 
     return (
-        <div className='w-full min-h-[350px] lg:h-[450px] flex flex-col items-center justify-center gap-6 bg-banner bg-cover bg-no-repeat bg-bottom rounded-lg p-6 lg:p-14'>
+        <div className='w-full min-h-[350px] lg:h-[450px] flex flex-col items-center justify-center gap-6 bg-banner bg-cover bg-no-repeat bg-bottom rounded-lg p-6 lg:p-14 mb-8'>
             <div className="max-w-[600px] lg:max-w-[730px] text-white font-bold capitalize text-center text-3xl sm:text-4xl lg:text-5xl md:leading-[45px] lg:leading-[55px]" data-aos='fade-zoom-in'>
                 <span>{text1}&nbsp;</span>
                 <span className="text-themeGold">{text2}&nbsp;</span>
