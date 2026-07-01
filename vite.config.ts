e14269938c4ca8a6731b7dff3c7390d1c00b0c78
pratitb <pratit.bangdiwala7@gmail.com>
2026-07-01 16:57:55 +0530

    fix: prod base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-	base: process.env.NODE_ENV === 'production' ? '/the-tile-theory/' : '/',
-	plugins: [react()],
+export default defineConfig(() => {
+	// Check if we are building for cPanel production
+	const isProd = process.env.VITE_DEPLOY_ENV === 'prod';
+
+	return {
+		base: isProd ? '/' : '/the-tile-theory/',
+		plugins: [react()],
+	};
 });
